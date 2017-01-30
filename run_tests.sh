d2bfcb80f7cd129168cad54c7cb3bebc25d237da
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2017-01-30 13:32:35 +0700

    update tests.sh submodule

--- run_tests.sh
+++ run_tests.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 _base_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source $_base_dir/vendor/github.com/reconquest/import.bash/import.bash
 
-import github.com/reconquest/test-runner.bash
+import:use github.com/reconquest/test-runner.bash
 
 test-runner:run "${@}"
